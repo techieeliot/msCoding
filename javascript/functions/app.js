1cddbca8270f2bc5c1c4c0c162676cc506273592
“[Eliot <[eliot587@gmail.com]>
2020-01-07 18:16:49 -0600

    day 11 mca bootcamp DOM

--- javascript/functions/app.js
+++ javascript/functions/app.js
@@ -114,7 +114,158 @@ convertTemperature(30, 'C');
     }
     
     grader(78);
+}
+{
+    // day 2 of functions
+
+    //template literals
+
+    name = 'Eliot'
+    console.log(`Hey, my name is ${name}`);
+
+    //FUNCTION EXPRESSIONS
+    const speaker = function() { // it's a good idea to use const
+        console.log('The speaker has spoken');
+        
+    }
     
+    speaker(); // must be invoked 
+
+    //hoisting 
+    hello('Eliot')
+    function hello(name) {
+        console.log(`Hey, my name is ${name}`);
+    }
+
+    // however you cannot hoist function expressions. If speaker is declared before line 126 let speaker... it will error. 
+
+    //OBJECT CONSTRUCTOR METHOD
+
+    function Person(fName, lName, age){
+        this.firstName = fName,
+        this.lastName = lName,
+        this.age = age;
+    }
+    
+    let eliot = new Person('Eliot', 'Sanford', 32);
+    let ranjita = new Person('Ranjita', 'Sanford', 30);
+
+    console.log(eliot, ranjita);
+    console.log(`Welcome to the Person object constructor method, ${eliot.firstName} and ${ranjita.firstName}`);
+    
+    // classes in javascript are just object constructor methods and syntactical sugar in javasript
+
+    //review the material in this book.
+
+    //SCOPE
+
+    let place = 'York';
+    function namePlace(name) {
+        let place = 'Orleans';
+        console.log(`${name} ${place}`);
+    }
+
+    namePlace('New');
+
+    function scope(param='Hello', param2 = 'World') {
+        var newVar = 'hola';
+        let newLet = 'mundo';
+        const newConst = "!";
+        console.log(param, param2);
+        
+    }
+    scope();
+    scope('Disney');
+    scope('Disney', 'Mundo');
+    // console.log(param, param2, newVar, newLet, newConst); // Each of these are function scoped and will not log in the global scope.
+    
+
+    function zoo(animals) {
+        console.log(animals);
+        
+    }
+    zoo(['Lions', 'Tigers', 'Bears']);
+
+    function farm() {
+        console.log(arguments); // arguments value allows you to output an undetermined number of item input parameters
+        console.log(arguments[0]); // outputs chickens
+    }
+
+    farm("Chickens", "Pig", "Dog", "Cow", "Duck", "Horse");
+
+    function shelf(...args) { // spread operator
+        console.log(args);
+    }
+
+    shelf('Bible', 'Concordance', 'Commentary');
+
+    function students(){
+        console.log([...arguments]); 
+    }
+    students("Walker", "Wright", "John Michael", "Casidee", "KK");
+
+    // stacks
+    // lifo // last in first out
+    // last place in is the first out of the stack of plates
+
+    function multipy(a, b) {
+        return a * b;
+    }
+
+    function square(n) {
+        return multipy(n, n);
+    }
+
+    function printSquare(n) {
+        let result = square(n);
+        console.log(result);
+    }
+
+    printSquare(9); 
+    /** Start with printSquare for result...
+         go to square.. 
+         go to multiply.. 
+         execute the operation of 81, then... 
+         return it to square, then...
+         return it to printSquare as result...
+         log the result of 81
+    */
+
+    // QUEUE
+    // fifo = first in first out
+    // queue is a line
+    // queues are much more efficient because people like them better when they get in first and get out first.
+
+    //EXERCISE
+    // WRITE A FUNCTIONS THAT DOES SIMPLE MATH
+    // ADD, SUBTRACT, DIVIDE, MULTIPLY AND RETURNS...
+    // THE RESULT
+    // I WOULD LIKE ADD TO BE THE DEFAULT RETURN
+
+    function simpleMath(num1, num2, func = "add") {
+        let result;
+
+            if (!num1 || !num2)
+                return "Result cannot populate without two numbers input."; // early return helps you avoid running unnecessary lines of code.
+
+            if (func == "divide") {
+                result = num1 / num2;
+            } else if (func == "multiply") {
+                result = num1 * num2;
+            } else if (func == "subract") {
+                result = num1 - num2;
+            } else {
+                result = num1 + num2;
+            }
+        console.log(result);
+        
+    }
+
+    simpleMath(1, 2);
+
+    
+
+
 
 
 }
