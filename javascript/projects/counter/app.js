0a369d84bb6550627cd140a20e9242bc2d81cf44
[Eliot Sanford] <eliot587@gmail.com>
2020-01-22 09:30:44 -0600

    day 16 mca working counter

---
+++ javascript/projects/counter/app.js
@@ -0,0 +1,18 @@
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+let display = document.getElementById('output');
+let counter = 0;
+
+plus.onclick = incrementer;
+minus.onclick = decrementer;
+
+function incrementer(){
+    counter+= 1;
+    display.innerText = counter;
+};
+
+function decrementer(){
+    counter-= 1;
+    display.innerText = counter;
+};
+
