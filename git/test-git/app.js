c2875a53c9b0abc03ae4cbe4b14d83602edc52f1
[Eliot Sanford] <eliot587@gmail.com>
2020-01-28 09:28:15 -0600

    fix test-git-init

---
+++ git/test-git/app.js
@@ -0,0 +1,17 @@
+// something in here
+
+function callMe(person) {
+    console.log(`Hey ${person}, call me!`);
+    
+}
+
+callMe('Jimmy');
+
+let thing = true;
+if (thing) {
+    console.log("The thing is true!");
+    //thing
+}
+
+//thing to update now again commit this
+// thing
